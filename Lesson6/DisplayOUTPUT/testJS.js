3a8e76ad5184a9b8f8a4dbf5944e583209dc4132
trantheanh3101 <trantheanhk11@gmail.com>
2024-04-06 17:16:22 +0700

    completL6

---
+++ Lesson6/DisplayOUTPUT/testJS.js
@@ -0,0 +1,12 @@
+document.write("chào bạn đến trang wed")
+document.getElementById("code1").innerText="<h1>Thay Truong Tan Hai day C0324I1</h1>"
+document.getElementById("code2").innerHTML="<h1>Thay Truong Tan Hai day C0324I1</h1>"
+
+function theanh() {
+    code = document.getElementById("code").value;
+    alert(code);
+}
+
+function nhinconsole() {
+        console.log('oánh nhau anh em ơi')
+}
