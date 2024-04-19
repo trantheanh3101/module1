5d821d09ad9fe92d50071251e402b2703834e154
trantheanh3101 <trantheanhk11@gmail.com>
2024-04-19 15:49:21 +0700

    t6

---
+++ Lesson15_Function2/Ex1_checkPrime.js
@@ -0,0 +1,25 @@
+function prime() {
+    let count=0;
+    let num = document.getElementById('num').value;
+    let start = document.getElementById('start').value;
+    let end = document.getElementById('end').value;
+
+    while (count<num && start<end){
+        if (checkPrime(start)){
+            document.write(start + " ");
+            count++;
+        }
+        start++;
+    }
+    document.getElementById('result').innerHTML = start;
+}
+function checkPrime(n){
+    //     tra ve true neu la so nguyen to
+    //     tra false neu ko la so nguyen to
+    if (n<2) return false;
+    if (n<4) return true;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n%i===0) return false;
+    }
+    return true;
+}
